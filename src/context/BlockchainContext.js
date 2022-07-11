05ebb47248570d832b7868eb54dece2f0114d063
mitullgadhiya <mrab0709@gmail.com>
2022-07-11 19:27:22 +0530

    added admin section

--- src/context/BlockchainContext.js
+++ src/context/BlockchainContext.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-//import { abi, contractAddress } from '../config.json';
+import { abi, contractAddress } from "../config.json";
 import { ethers } from "ethers";
 import { toast } from "react-toastify";
 
@@ -7,369 +7,21 @@ export const BlockchainContext = React.createContext("");
 
 export const BlockchainProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
-  const [ setBalance] = useState();
+  const [balance, setBalance] = useState(0);
+  const [ownerBalance, setOwnerBalance] = useState();
   const [renterExists, setRenterExists] = useState();
   const [renter, setRenter] = useState();
   const [renterBalance, setRenterBalance] = useState();
   const [due, setDue] = useState();
   const [duration, setDuration] = useState();
+  const [owner, setOwner] = useState(false);
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
 
-  const address = "0xFbF4465872db3133b56F4F47972669251C676405";
-  const abi = [
-    {
-      inputs: [],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address payable",
-          name: "walletAddress",
-          type: "address",
-        },
-        {
-          internalType: "string",
-          name: "firstName",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "lastName",
-          type: "string",
-        },
-        {
-          internalType: "bool",
-          name: "canRent",
-          type: "bool",
-        },
-        {
-          internalType: "bool",
-          name: "active",
-          type: "bool",
-        },
-        {
-          internalType: "uint256",
-          name: "balance",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "due",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "start",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "end",
-          type: "uint256",
-        },
-      ],
-      name: "addRenter",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "balanceOf",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "balanceOfRenter",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "canRentBike",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "checkIn",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "checkOut",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "deposit",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "getDue",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "getOwnerBalance",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "getRenter",
-      outputs: [
-        {
-          internalType: "string",
-          name: "firstName",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "lastName",
-          type: "string",
-        },
-        {
-          internalType: "bool",
-          name: "canRent",
-          type: "bool",
-        },
-        {
-          internalType: "bool",
-          name: "active",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "getTotalDuration",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "isOwner",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "amount",
-          type: "uint256",
-        },
-      ],
-      name: "makePayment",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "walletAddress",
-          type: "address",
-        },
-      ],
-      name: "renterExists",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "",
-          type: "address",
-        },
-      ],
-      name: "renters",
-      outputs: [
-        {
-          internalType: "address payable",
-          name: "walletAddress",
-          type: "address",
-        },
-        {
-          internalType: "string",
-          name: "firstName",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "lastName",
-          type: "string",
-        },
-        {
-          internalType: "bool",
-          name: "canRent",
-          type: "bool",
-        },
-        {
-          internalType: "bool",
-          name: "active",
-          type: "bool",
-        },
-        {
-          internalType: "uint256",
-          name: "balance",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "due",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "start",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "end",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "withdrawOwnerBalance",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-  ];
-  const contract = new ethers.Contract(address, abi, signer);
+  const address = contractAddress;
+  const contractAbi = abi;
+  const contract = new ethers.Contract(address, contractAbi, signer);
 
   const connectWallet = async () => {
     try {
@@ -399,14 +51,49 @@ export const BlockchainProvider = ({ children }) => {
     }
   };
 
-  // const getBalance = async () => {
-  //   try {
-  //     const contractBalance = await contract.balanceOf();
-  //     setBalance(ethers.utils.formatEther(contractBalance));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const getBalance = async () => {
+    try {
+      if (owner) {
+        const contractBalance = await contract.balanceOf();
+        setBalance(ethers.utils.formatEther(contractBalance));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getOwnerBalance = async () => {
+    try {
+      if (owner) {
+        const contractBalance = await contract.getOwnerBalance();
+        setOwnerBalance(ethers.utils.formatEther(contractBalance));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const ownerWithdraw = async () => {
+    try {
+      const ownerBalance = await contract.withdrawOwnerBalance();
+      await ownerBalance.wait();
+      await getOwnerBalance();
+      await getBalance();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const isOwner = async () => {
+    try {
+      if (currentAccount) {
+        const owner = await contract.isOwner();
+        setOwner(owner);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const checkRenterExists = async () => {
     try {
@@ -558,7 +245,10 @@ export const BlockchainProvider = ({ children }) => {
     getRenterBalance();
     getDue();
     getTotalDuration();
-  }, [currentAccount]);
+    getOwnerBalance();
+    isOwner();
+    getBalance();
+  }, [currentAccount, owner]);
 
   return (
     <BlockchainContext.Provider
@@ -575,6 +265,10 @@ export const BlockchainProvider = ({ children }) => {
         makePayment,
         checkOut,
         checkIn,
+        ownerBalance,
+        ownerWithdraw,
+        balance,
+        owner,
       }}
     >
       {children}
